34e54297ca00727334a100a851b9c89982fa7ce2
waldir developer <wmerjildoubaldo@gmail.com>
2021-07-13 21:09:13 -0500

    creacion aplicacion se suma de numeros

---
+++ js/suma.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var x,y,suma,text;
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+    if (isNaN(x) || isNaN(y)) {
+        text = "Es necesarios introducir dos números válidos";
+    } else{
+      //si no ponemos parseFloat concatenaría x con y
+        suma=parseFloat(x)+parseFloat(y);
+        text= suma;
+    }
+    document.getElementById("sumando").innerHTML = text;
+    
+}
